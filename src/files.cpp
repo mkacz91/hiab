06c0cf7e34a7532fa8672957e8d4c0d09163db97
Marcin Kaczmarek <mkc@orgadata.com>
2017-12-23 20:25:25 +0100

    Expand file I/O framework

--- src/files.cpp
+++ src/files.cpp
@@ -3,30 +3,56 @@
 
 namespace hiab {
 
-std::vector<string> file_search_prefixes = { "" };
+std::vector<string> file_search_prefixes = { "." };
 
-file_not_found::file_not_found(string const& name) :
-    runtime_error("File " + squote(name) + " could not be found."),
+file_error::file_error(string const& name, string const& message) :
+    runtime_error("Error loading file " + squote(name) + ": " + message),
     m_name(name)
 { }
 
-string const& file_not_found::name() const { return m_name; }
+string const& file_error::name() const { return m_name; }
+
+file_not_found::file_not_found(string const& name) :
+    file_error(name, "file not found.")
+{ }
+
+string join_path(string const& path0, string const& path1)
+{
+    return path0 + FILE_PATH_SEPARATOR + path1;
+}
 
 void add_file_search_prefix(string const& prefix)
 {
     file_search_prefixes.push_back(prefix);
 }
+template <typename Action>
+void dispatch_on_located_file(string const& name, Action action)
+{
+    std::ifstream stream;
+    for (string const& prefix : file_search_prefixes)
+    {
+        string path = join_path(prefix, name);
+        stream.open(path);
+        if (stream.is_open())
+            return action(std::move(path), std::move(stream));
+    }
+    throw file_not_found(name);
+}
+
+string get_file_path(string const& name)
+{
+    string result;
+    dispatch_on_located_file(name, [&](string&& name, std::ifstream const& _)
+        { std::swap(result, name); });
+    return std::move(result);
+}
 
 std::ifstream open_file_for_reading(string const& name)
 {
     std::ifstream result;
-    for (string const& prefix : file_search_prefixes)
-    {
-        result.open(prefix + FILE_PATH_SEPARATOR + name);
-        if (result.is_open())
-            return std::move(result);
-    }
-    throw file_not_found(name);
+    dispatch_on_located_file(name, [&](string const& _, std::ifstream&& stream)
+        { std::swap(result, stream); });
+    return std::move(result);
 }
 
 string read_all_text_from_file(string const& name)
--- src/files.h
+++ src/files.h
@@ -11,17 +11,27 @@ namespace hiab {
     constexpr char FILE_PATH_SEPARATOR = '/';
 #endif
 
-class file_not_found : public std::runtime_error
+class file_error : public std::runtime_error
 {
 public:
-    explicit file_not_found(string const& name);
+    file_error(string const& name, string const& message);
     string const& name() const;
 
 private:
     string m_name;
 };
 
+class file_not_found : public file_error
+{
+public:
+    explicit file_not_found(string const& name);
+};
+
+string join_path(string const& path0, string const& path1);
+
 void add_file_search_prefix(string const& prefix);
+
+string get_file_path(string const& name);
 
 std::ifstream open_file_for_reading(string const& name);
 
