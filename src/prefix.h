f299650921da0c3c874a5d196af98c15c1d79c5f
Marcin Kaczmarek <mkc@orgadata.com>
2017-12-23 20:26:17 +0100

    Add not_implemented exception

--- src/prefix.h
+++ src/prefix.h
@@ -14,4 +14,13 @@ inline string quote(const string& s) { return "\"" + s + "\""; }
 inline string squote(const string& s) { return "'" + s + "'"; }
 inline string paren(const string& s) { return "(" + s + ")"; }
 
+class not_implemented : public std::logic_error
+{
+public:
+    not_implemented(string const& message) : logic_error("Not implemented: " + message) { }
+};
+
+#define not_implemented_here not_implemented( \
+    "line " + to_string(__LINE__) + ", file '" __FILE__ "', ")
+
 } // namespace hiab
