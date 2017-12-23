3f3fb2fe1ed36b4f847d63d86c56698771f8dc55
Marcin Kaczmarek <mkc@orgadata.com>
2017-12-23 20:20:54 +0100

    Ditch custom to_str implementation
    STL already has one

--- src/prefix.h
+++ src/prefix.h
@@ -4,22 +4,14 @@
 #include <stdexcept>
 #include <utility>
 #include <string>
-#include <sstream>
 
 namespace hiab {
 
 using std::string;
+using std::to_string;
 
 inline string quote(const string& s) { return "\"" + s + "\""; }
 inline string squote(const string& s) { return "'" + s + "'"; }
 inline string paren(const string& s) { return "(" + s + ")"; }
-
-template <typename T> inline
-std::string to_string(const T& t)
-{
-    std::ostringstream sout;
-    sout << t;
-    return sout.str();
-}
 
 } // namespace hiab
